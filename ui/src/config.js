ce3a387c4f7f73660f4732d310e495802e0fc40b
Andrii Taran <tandryukha@gmail.com>
2025-09-01 15:35:23 +0300

    UI v2

--- ui/src/config.js
+++
@@ -1,4 +0,0 @@
-export const API_BASE =
-  new URLSearchParams(location.search).get('api') || 'http://localhost:4000';
-
-
--- ui/src/core/bus.js
+++
@@ -1,5 +0,0 @@
-// Simple app-wide event bus
-export const bus = new EventTarget();
-// usage: bus.dispatchEvent(new CustomEvent('event', { detail: {...} }))
-
-
